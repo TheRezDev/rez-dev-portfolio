79b558a636e71f6f549d590622919973db675526
Reza.dev <the.rez.dev@gmail.com>
2023-08-22 09:07:09 +0330

    feature/scroll bar + scroll up button +  active link menu  @ mobile first

--- assets/js/main.js
+++ assets/js/main.js
@@ -69,11 +69,35 @@ const sendEmail = (e) => {
 contactForm.addEventListener('submit', sendEmail);
 
 
-/*=============== SHOW SCROLL UP ===============*/ 
+/*=============== SHOW SCROLL UP ===============*/
 
-
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up')
+    //when the scroll is higher than 350 veiwport height, add the show-scroll class to the a tag with scrollup class 
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                        : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp);
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () => {
+    const scrollDown = window.scrollY
 
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id'),
+            sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+        if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+            sectionsClass.classList.add('active-link')
+        } else {
+            sectionsClass.classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
 
 /*=============== DARK LIGHT THEME ===============*/ 
 
